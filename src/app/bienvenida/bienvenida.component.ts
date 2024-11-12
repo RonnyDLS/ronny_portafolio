4bba9e5abe93d46d80a410773bb011da79af7e20
Ronny De Los Santos <delossantosr835@gmail.com>
2024-11-12 09:42:32 -0400

    Sustitusion de video por gif en la pantalla de bienvenida

--- src/app/bienvenida/bienvenida.component.ts
+++ src/app/bienvenida/bienvenida.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component} from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { ConexionRutasComponent } from "../conexion-rutas/conexion-rutas.component";
+import { error } from 'node:console';
 
 @Component({
   selector: 'app-bienvenida',
@@ -9,18 +10,23 @@ import { ConexionRutasComponent } from "../conexion-rutas/conexion-rutas.compone
   imports: [
     CommonModule,
     ConexionRutasComponent
-],
+  ],
   templateUrl: './bienvenida.component.html',
   styleUrl: './bienvenida.component.css'
 })
-export class BienvenidaComponent {
+export class BienvenidaComponent implements OnInit {
+  
+  
+  @ViewChild('video') videoref!: ElementRef<HTMLVideoElement>;
 
-  mostrarBienvenida:boolean= true;
-  mostrarNav:boolean = false;
-
-  evantoEnded(){
-    this.mostrarBienvenida = false;
-    this.mostrarNav = true;
+  mostrarBienvenida: boolean = true;
+  mostrarNav: boolean = false;
+  
+  ngOnInit(): void {
+    setTimeout(()=>{
+      this.mostrarBienvenida = false;
+      this.mostrarNav = true;
+    }, 4000)
   }
 
 }
