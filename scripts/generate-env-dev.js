563f3e4ec3464cc659eefcf325fcc81e3cf62928
Ronny De Los Santos <delossantosr835@gmail.com>
2026-01-02 02:39:22 -0400

    fix

--- scripts/generate-env-dev.js
+++ scripts/generate-env-dev.js
@@ -9,7 +9,7 @@ dotenv.config();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const targetPath = path.join(__dirname, '../src/environments/environment.prod.ts');
+const targetPath = path.join(__dirname, '../src/environments/environment.ts');
 
 // Asegurarte que la carpeta existe
 const envDir = path.dirname(targetPath);
